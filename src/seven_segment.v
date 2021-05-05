594d5e2c376f15c8df27109222e062f08feba7dc
Konrad Rzeszutek Wilk <konrad@kernel.org>
2021-05-05 07:58:38 +0200

    seven_segment: Initial implementation.
    Interesting observation, if we initialize on the
    decleration line the copy_[ten_count,unit_count]
    to the value of zero, then it stays in Z for the life,
    even if in reset we set it to zero. Unless the reset
    had other values (like 2, or 3).
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad@kernel.org>

--- src/seven_segment.v
+++ src/seven_segment.v
@@ -1,5 +1,32 @@
 `default_nettype none
 `timescale 1ns/1ps
+
+
+module seg7 (
+    input wire [3:0] counter,
+    output reg [6:0] segments
+);
+
+	always @(*) begin
+        case(counter)
+            //                7654321
+            0:  segments = 7'b0111111;
+            1:  segments = 7'b0000110;
+            2:  segments = 7'b1011011;
+            3:  segments = 7'b1001111;
+            4:  segments = 7'b1100110;
+            5:  segments = 7'b1101101;
+            6:  segments = 7'b1111100;
+            7:  segments = 7'b0000111;
+            8:  segments = 7'b1111111;
+            9:  segments = 7'b1100111;
+            default:
+                segments = 7'b0000000;
+        endcase
+    end
+
+endmodule
+
 module seven_segment (
     input wire          clk,
     input wire          reset,
@@ -10,4 +37,31 @@ module seven_segment (
     output reg          digit
 );
 
+	reg [3:0] copy_ten_count;
+	reg [3:0] copy_unit_count;
+	reg select_output = 0;
+	wire [3:0] values;
+
+	assign values = (select_output ? copy_unit_count : copy_ten_count);
+
+	always @(posedge clk) begin
+			if (reset) begin
+					select_output <= 0;
+					copy_ten_count <= 0;
+					copy_unit_count <= 0;
+					digit <= 0;
+			end else
+				// Flips back and forth.
+				select_output <= !select_output;
+	end
+
+	always @(posedge load) begin
+		copy_ten_count <= ten_count;
+		copy_unit_count <= unit_count;
+	end
+	always @(select_output) begin
+		digit <= select_output;
+	end
+
+	seg7 seg7(.counter(values), .segments(segments));
 endmodule
