09f0c684df669fc1af7ca8ef43b69216d68664d4
Konrad Rzeszutek Wilk <konrad@kernel.org>
2021-05-05 22:38:02 +0200

    frequency_counter: Initial implementation.
    Which seems to work!
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad@kernel.org>

--- src/frequency_counter.v
+++ src/frequency_counter.v
@@ -17,42 +17,87 @@ module frequency_counter #(
     output wire             digit
     );
 
+    localparam LOCAL_BITS = 7;
+    localparam LOCAL_SEGMENTS = 3;
     // states
     localparam STATE_COUNT  = 0;
     localparam STATE_TENS   = 1;
     localparam STATE_UNITS  = 2;
-
+    localparam STATE_FLUSH  = 3;
     reg [2:0] state = STATE_COUNT;
+    reg [LOCAL_BITS:0] edge_counter;
+    reg [LOCAL_SEGMENTS:0] ten_count;
+    reg [LOCAL_SEGMENTS:0] unit_count;
+    reg [BITS:0] clk_counter;
+
+    reg load_enable;
+    wire leading_edge_detect;
+
+    edge_detect Edge(.clk(clk),
+                     .signal(signal),
+                     .leading_edge_detect(leading_edge_detect));
+
+    seven_segment SevenSegment(.clk(clk),
+                               .reset(reset),
+                               .load(load_enable),
+                               .ten_count(ten_count),
+                               .unit_count(unit_count),
+                               .segments(segments),
+                               .digit(digit));
 
     always @(posedge clk) begin
         if(reset) begin
-            
-            // reset things here
-
+            state <= STATE_COUNT;
+            load_enable <= 1'b0;
+	    edge_counter <= 0;
+            clk_counter <= 0;
+            ten_count <= 0;
+            unit_count <= 0;
         end else begin
             case(state)
                 STATE_COUNT: begin
                     // count edges and clock cycles
-
+                    clk_counter <= clk_counter + 1;
+                    if (leading_edge_detect) begin
+                            if (edge_counter < 99) begin
+                                edge_counter <= edge_counter + 1;
+                            end
+                    end
                     // if clock cycles > UPDATE_PERIOD then go to next state
+                    if (clk_counter > UPDATE_PERIOD) begin
+                            state <= STATE_TENS;
+                    end
                 end
 
                 STATE_TENS: begin
                     // count number of tens by subtracting 10 while edge counter >= 10
-
+                    if (edge_counter >= 10) begin
+                            edge_counter <= edge_counter - 10;
+                            ten_count <= ten_count + 1;
+                    end else
                     // then go to next state
+                            state <= STATE_UNITS;
                 end
 
                 STATE_UNITS: begin
                     // what is left in edge counter is units
-
+                    unit_count <= edge_counter;
                     // update the display
-
+                    load_enable <= 1'b1;
                     // go back to counting
+                    state <= STATE_FLUSH;
                 end
 
+                STATE_FLUSH: begin
+                    unit_count <= 0;
+                    ten_count <= 0;
+                    load_enable <= 1'b0;
+		    edge_counter <= 0;
+		    clk_counter <= 0;
+                    state          <= STATE_COUNT;
+                end
                 default:
-                    state           <= STATE_COUNT;
+                        state <= STATE_COUNT;
 
             endcase
         end
